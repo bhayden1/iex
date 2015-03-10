9a88be68d38c6a70c270a3d98111fd37506486ec
bhayden1 <bhayden1@gmail.com>
2015-03-10 17:04:11 -0500

    adding unit test project

---
+++ iex.test/ExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace iex.test
+{
+  [TestFixture]
+  public class ExtensionsTests
+  {
+    [TestCase("Name")]
+    [TestCase("Company")]
+    [TestCase("Number")]
+    [TestCase("Active")]
+    [TestCase("Name,Company,Number,Active")]
+    public void ToCsv_Adds_Headers_When_Asked(string expected)
+    {
+      var data = GenerateData();
+      var results = data.ToCsv(true);
+      results.Should().Contain(expected);
+    }
+
+    [TestCase("True")]
+    [TestCase("Company 1")]
+    [TestCase("Name 1")]
+    [TestCase("100")]
+    [TestCase("Name 1,Company 1,100,True")]
+    public void ToCsv_Adds_Comma_Delimited_Data(string expected)
+    {
+      var data = GenerateData();
+      var results = data.ToCsv(true);
+      results.Should().Contain(expected);
+    }
+
+    public List<ExtensionTest> GenerateData()
+    {
+      return new List<ExtensionTest>
+      {
+        new ExtensionTest {Active = true, Company = "Company 1", Name = "Name 1", Number = 100},
+      };
+    }
+  }
+
+  public class ExtensionTest
+  {
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public int Number { get; set; }
+    public bool Active { get; set; }
+  }
+}
---
+++ iex.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("iex.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("iex.test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef48e1eb-5806-4687-b0da-a6c00a44aa66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
