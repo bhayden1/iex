b616181cb55974a6a6945a2fe81ef7eeb47d65bf
bhayden1 <bhayden1@gmail.com>
2015-03-10 17:01:38 -0500

    Adding first extension.

---
+++ Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastMember;
+
+namespace iex
+{
+  public static class Extensions
+  {
+    public static string ToCsv<T>(this IEnumerable<T> list, bool withHeaders)
+    {
+      var builder = new StringBuilder();
+      var type = typeof(T);
+      var properties = type.GetProperties().Select(r => r.Name).ToList();
+      if (withHeaders)
+      {
+        var headers = string.Join(",", properties);
+        builder.AppendLine(headers);
+      }
+      
+      list.Select(r => ObjectAccessor.Create(r))
+        .ToList()
+        .ForEach(r =>
+        {
+          var values = properties.Select(property => r[property].ToString()).ToList();
+          var csvValues = string.Join(",", values);
+          builder.AppendLine(csvValues);
+        });
+
+      return builder.ToString();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iex
 Naive implementation of some IEnumerable Extensions
+
+ToCsv(bool withHeaders)
+-----
+Takes an IEnumerable and converts it into a CSV string.  Will add headers using the property name as the header if withHeaders is true.
